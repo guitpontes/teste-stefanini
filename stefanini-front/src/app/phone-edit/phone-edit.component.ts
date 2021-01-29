db802cac52fac7e723db80b518c4b805f4935cc9
guitpontes <43714700+guitpontes@users.noreply.github.com>
2021-01-29 12:12:14 -0300

    Ajusta edição de telefone

--- stefanini-front/src/app/phone-edit/phone-edit.component.ts
+++ stefanini-front/src/app/phone-edit/phone-edit.component.ts
@@ -67,7 +67,7 @@ export class PhoneEditComponent implements OnInit {
     var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
     var match = cleaned.match(/^(\d{2})(\d{5})(\d{4})$/)
     if (match) {
-      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+      return '(' + match[1] + ')' + match[2] + '-' + match[3]
     }
     return null
   }
